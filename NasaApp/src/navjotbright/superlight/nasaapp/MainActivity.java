b07f4bf0e06a4078a1525f805f93d6ce85334244
Virksaabnavjot <virksaabnavjot@gmail.com>
2015-01-02 22:33:20 +0000

    Commit 2

--- NasaApp/src/navjotbright/superlight/nasaapp/MainActivity.java
+++ NasaApp/src/navjotbright/superlight/nasaapp/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	/*hello ji ji*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
