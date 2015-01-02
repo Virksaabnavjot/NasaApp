83559e4c19178601ed341fc7d315ed01241a4658
Virksaabnavjot <virksaabnavjot@gmail.com>
2015-01-02 22:39:50 +0000

    Comit 3

--- NasaApp/src/navjotbright/superlight/nasaapp/MainActivity.java
+++ NasaApp/src/navjotbright/superlight/nasaapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 	
-	/*hello ji ji*/
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,3 +22,30 @@ public class MainActivity extends Activity {
 	}
 
 }
+/*
+ * package navjotbright.superlight.nasaapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+ 
+*/
